58e85b2dfed59cc021ed296a0ece721dbed6c552
Daniel Nguyen <233656241+DucAnh0106@users.noreply.github.com>
2026-02-24 15:10:27 +0800

    rewrite vite to access ESP web server in ESP's network

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Change this to your repo name for GitHub Pages deployment
   base: '/Wander_Bin-App/',
+  server: {
+    proxy: {
+      '/api/esp32': {
+        target: 'http://192.168.4.1',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/esp32/, '')
+      }
+    }
+  }
 })
